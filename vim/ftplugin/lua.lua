a8b0a1f1967926785aab31c67700d226a868ddd2
chimre <github@reev.jp>
2021-10-17 09:53:03 +0900

    improve vim configure ftplugin

---
+++ vim/ftplugin/lua.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_buf_set_option(0, 'expandtab', true)
+vim.api.nvim_buf_set_option(0, 'shiftwidth', 2)
+vim.api.nvim_buf_set_option(0, 'softtabstop', 2)
+vim.api.nvim_buf_set_option(0, 'tabstop', 2)
+vim.api.nvim_buf_set_option(0, 'autoindent', false)
+vim.api.nvim_buf_set_option(0, 'smartindent', false)
+
