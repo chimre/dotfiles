2e90eba23a5c28b7b1f542ca1c591c5ec14d245b
chimre <25086631+chimre@users.noreply.github.com>
2021-11-28 10:52:02 +0900

    install vscode and porting neovim to vscode-neovim (#21)

--- nvim/ftplugin/python.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_buf_set_option(0, 'expandtab', true)
-vim.api.nvim_buf_set_option(0, 'shiftwidth', 2)
-vim.api.nvim_buf_set_option(0, 'softtabstop', 2)
-vim.api.nvim_buf_set_option(0, 'tabstop', 2)
-vim.api.nvim_buf_set_option(0, 'autoindent', false)
-vim.api.nvim_buf_set_option(0, 'smartindent', false)
-
--- nvim/lua/helper.lua
+++ nvim/lua/helper.lua
@@ -74,4 +74,3 @@ M.executable = function (name)
 end
 
 return M
-
